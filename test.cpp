05382db86793c9755720f724206cad6aaec8463c
Veselin <73367211+veselinkaramfilov@users.noreply.github.com>
2022-06-21 21:09:35 +0100

    updated

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Veselin Karamfilov on 21/06/2022.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,16 @@
+//
+// Created by Veselin Karamfilov on 21/06/2022.
+//
+
+#ifndef SWAP_YE_TEST_H
+#define SWAP_YE_TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //SWAP_YE_TEST_H
