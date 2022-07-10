87e1b95844c7a0e6d877db9016df90aedfc30244
Veselin <73367211+veselinkaramfilov@users.noreply.github.com>
2022-07-10 18:50:27 +0100

    10 july

--- app/src/main/java/com/example/teamproject/SignIn.java
+++ app/src/main/java/com/example/teamproject/SignIn.java
@@ -1,14 +1,59 @@
 package com.example.teamproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
+
+    EditText email, password;
+    Button logIn;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        logIn = findViewById(R.id.logIn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            if(mAuth.getCurrentUser().isEmailVerified()) {
+                                startActivity(new Intent(SignIn.this, Profile.class));
+                            }
+                            else {
+                                    Toast.makeText(SignIn.this, "Please verify your email address", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(SignIn.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+            }
+        });
     }
 }
